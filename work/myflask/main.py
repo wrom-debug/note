ac0b763b7aaa0d4707c41d46807084d26e2729e4
worm-debug <245774348@qq.com>
2021-07-20 10:30:05 +0800

    在登录后做session校验，并且添加上传文件功能

--- work/myflask/main.py
+++ work/myflask/main.py
@@ -1,6 +1,7 @@
+import uuid
 from flask import Flask,render_template,request,session,redirect,send_file
 from pymongo import MongoClient
-
+import os
 
 
 mc=MongoClient("127.0.0.1",27017)
@@ -8,6 +9,19 @@ db=mc['xs']
 app = Flask(__name__)
 app.config['SECRET_KEY']='78912312#KLH$@KHkhdad'
 
+@app.before_request
+def before():
+    if  not request.path in ["/static/02.jpg","/static/jquery.min.js","/static/dl.css","/dl","/zc","/"]:
+        if session:
+            use=session['use']
+            if use:
+                return None
+            else:
+                return "请先登录"
+        else:
+            return "请先登录"
+    else:
+        None
 @app.route("/")
 def index():
     return render_template('index.html')
@@ -38,13 +52,26 @@ def zc():
         a=request.form.to_dict()
         print(a)
         xs_use_crea(a["user"],a['password'])
-        return "注册成功"
+        return redirect("/")
+
+@app.route("/xs")
+def xs():
+    return send_file("static/1.txt")
+@app.route("/lswj",methods=['POST','GET'])
+def lswj():
+    if request.method== "GET":
+        return render_template("lswj.html")
+    else: 
+        name=str(uuid.uuid4())
+        print(name)
+        os.mkdir("work/myflask/xs/"+name)
+        data=request.files["wj"]
+        data.save("work/myflask/xs/"+name+"/"+data.filename)
+        return redirect("/home")
 
 @app.route('/home')
 def home():
     use=session['use']
-    return send_file("static/1.txt")
-
-
+    return render_template("home.html",use=use)
 if __name__ == "__main__":
     app.run()
