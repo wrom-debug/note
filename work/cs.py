0211e9f0ae739db1b164cef35a322f2ddd2eb062
worm-debug <245774348@qq.com>
2021-07-28 23:48:05 +0800

    算法示例题

---
+++ work/cs.py
@@ -0,0 +1,14 @@
+def cs(l:list):
+    c=0
+    while True:
+        l.sort(reverse=True)
+        print(l)
+        if l.count(l[0])==len(l):
+            break
+        l=list(map(lambda x:x+1,l))
+        l[0]=l[0]-1
+        c=c+1
+    return c
+
+g=cs([1,2,3,5])
+print(g)
